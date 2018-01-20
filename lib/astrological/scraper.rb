4fc64089c611512a993faac5774fb4832339b13c
Andy Maskiell <maskiella@gmail.com>
2018-01-20 03:44:59 +0000

    Moved scraper function inside of sign

--- lib/astrological/scraper.rb
+++ lib/astrological/scraper.rb
@@ -1,16 +1,17 @@
-class Astrological::Scraper
-
-  def get_page_one
-    Nokogiri::HTML(open("http://www.astrology-zodiac-signs.com/"))
-  end
-
-  def page_one_sign_names
-    self.get_page_one.css("#zodiac-signs .tittle .portfolio-2").text
-  end
-
-  def page_one_readings
-  end
-
-  def get_page_two
-  end
-end
+# class Astrological::Scraper
+#
+#   def get_page_one
+#     Nokogiri::HTML(open("http://www.astrology-zodiac-signs.com/"))
+#   end
+#
+#   def page_one_sign_names
+#     self.get_page_one.css("#zodiac-signs .tittle .portfolio-2").text
+#   end
+#
+#   def page_one_daily_reading
+#
+#   end
+#
+#   def get_page_two
+#   end
+# end
--- lib/astrological/sign.rb
+++ lib/astrological/sign.rb
@@ -1,8 +1,39 @@
 class Astrological::Sign
-  attr_accessor :name
+  attr_accessor :name, :daily, :daily_url, :weekly, :weekly_url, :monthly, :monthly_url, :yearly, :yearly_url
 
   def initialize(name = nil)
     @name = name
+  end
+
+  def self.scrape_sites
+    pages = []
+
+    pages << self.scrape_site_one
+    # pages << self.scrape_site_two
+
+    pages
+  end
+
+  def self.scrape_site_one # www.astrology-zodiac-signs.com/
+    site_one_doc = Nokogiri::HTML(open("http://www.astrology-zodiac-signs.com/"))
+
+    new_sign = self.new
+    new_sign.name = site_one_signs.css("#zodiac-signs .tittle .portfolio-2").text
+
+    site_one_sign_doc = Nokogiri::HTML(open("http://www.astrology-zodiac-signs.com/zodiac-signs/#{new_sign.name}"))
+    # Figure out how to scrape info from 'data-w-tab="Totady"'
+    binding.pry
+    # new_sign.daily =
+    # new_sign.daily_url =
+    # new_sign.weekly =
+    # new_sign.weekly_url =
+    # new_sign.monthly =
+    # new_sign.monthly_url =
+    # new_sign.yearly =
+    # new_sign.yearly_url =
+  end
+
+  def self.scrape_site_two # www.astrology.com/
   end
 
   # http://www.astrology-zodiac-signs.com/zodiac-sign/#{name}/
