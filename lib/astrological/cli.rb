8bed61291794059a4b0e91c7cfb6784027c7769f
Andy Maskiell <maskiella@gmail.com>
2018-01-20 01:23:01 +0000

    Beginnings of CLI logic

--- lib/astrological/cli.rb
+++ lib/astrological/cli.rb
@@ -10,6 +10,22 @@ class Astrological::CLI
     puts ""
     puts "Which sign would you like to see more info about? Please enter one of the following:"
     puts "Aquarius / Pisces / Aries / Taurus / Gemini / Cancer / Leo / Virgo / Libra / Scorpio / Sagittarius / Capricorn"
+    sign_valid? unless (sign_valid? == true)
+    # reading_valid? unless (reading_valid? == true)
+
+  end
+
+  def sign_valid?
+    puts ""
+    puts "Which sign would you like to see more info about? Please enter one of the following:"
+    puts "Aquarius / Pisces / Aries / Taurus / Gemini / Cancer / Leo / Virgo / Libra / Scorpio / Sagittarius / Capricorn"
+    sign_input = gets.strip.capitalize
+    if sign_input == ("Aquarius" || "Pisces" || "Aries" || "Taurus" || "Gemini" || "Cancer" || "Leo" || "Virgo" || "Libra" || "Scorpio" || "Sagittarius" || "Capricorn")
+      true
+    else
+      puts "I'm sorry, please try again."
+      sign_valid?
+    end
   end
 
 end
