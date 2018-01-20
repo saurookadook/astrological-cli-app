8cd471d4059bc51623e7bcbe6108677730e10749
Andy Maskiell <maskiella@gmail.com>
2018-01-20 01:46:55 +0000

    Established basic CLI logic

--- lib/astrological/cli.rb
+++ lib/astrological/cli.rb
@@ -9,9 +9,6 @@ class Astrological::CLI
   end
 
   def start
-    # puts ""
-    # puts "Which sign would you like to see more info about? Please enter one of the following:"
-    # puts "Aquarius / Pisces / Aries / Taurus / Gemini / Cancer / Leo / Virgo / Libra / Scorpio / Sagittarius / Capricorn"
     sign_valid?
     reading_valid?
 
@@ -22,7 +19,8 @@ class Astrological::CLI
     puts "Which sign would you like to see more info about? Please enter one of the following:"
     puts "Aquarius / Pisces / Aries / Taurus / Gemini / Cancer / Leo / Virgo / Libra / Scorpio / Sagittarius / Capricorn"
     sign_input = gets.strip.capitalize
-    if sign_input == ("Aquarius" || "Pisces" || "Aries" || "Taurus" || "Gemini" || "Cancer" || "Leo" || "Virgo" || "Libra" || "Scorpio" || "Sagittarius" || "Capricorn")
+    # Refactor with all signs kept in SIGNS_ARRAY constant and iterate over array to check for validity?
+    if (sign_input == "Aquarius") || (sign_input == "Pisces") || (sign_input == "Aries") || (sign_input == "Taurus") || (sign_input == "Gemini") || (sign_input == "Cancer") || (sign_input == "Leo") || (sign_input == "Virgo") || (sign_input == "Libra") || (sign_input == "Scorpio") || (sign_input == "Sagittarius") || (sign_input == "Capricorn")
       @chosen_sign = sign_input
     else
       puts "I'm sorry, the expression you entered is invalid. Please try again."
@@ -35,7 +33,7 @@ class Astrological::CLI
     puts "Great! Now, which reading would you like to view? Please enter one of the following:"
     puts "daily / weekly / monthly / yearly"
     reading_input = gets.strip.downcase
-    if reading_input == ("daily" || "weekly" || "monthly" || "yearly")
+    if (reading_input == "daily") || (reading_input == "weekly") || (reading_input == "monthly") || (reading_input == "yearly")
       @chosen_reading = reading_input
     else
       puts "I'm sorry, the expression you entered is invalid. Please try again."
