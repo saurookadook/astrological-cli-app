99de225280b0c90dd4cf03e105925d6218456e2c
Andy Maskiell <maskiella@gmail.com>
2018-01-26 19:40:14 +0000

    Scraper clean up

--- lib/astrological/scraper.rb
+++ lib/astrological/scraper.rb
@@ -1,9 +1,10 @@
 class Astrological::Scraper
   # attr_accessor :name, :daily, :daily_url, :weekly, :weekly_url, :monthly, :monthly_url, :yearly, :yearly_url
-  attr_accessor :name
+  attr_accessor :name, :current_year
 
   def self.scrape_sites(name)
     @name = name
+    @current_year = Time.new.year
     sign_info = []
 
     sign_info << self.scrape_site_one
@@ -16,7 +17,7 @@ class Astrological::Scraper
   def self.scrape_site_one # www.astrology-zodiac-signs.com/
     daily_doc = Nokogiri::HTML(open("http://www.astrology-zodiac-signs.com/horoscope/#{@name.downcase}/daily/"))
     monthly_doc = Nokogiri::HTML(open("http://www.astrology-zodiac-signs.com/horoscope/#{@name.downcase}/monthly/"))
-    yearly_doc = Nokogiri::HTML(open("http://www.astrology-zodiac-signs.com/horoscope/#{@name.downcase}/2018/")) # Use current year instead?
+    yearly_doc = Nokogiri::HTML(open("http://www.astrology-zodiac-signs.com/horoscope/#{@name.downcase}/#{@current_year}/")) # Use current year instead?
 
     # site_one_info = self.new
     # site_one_info.daily = daily_doc.css(".dailyHoroscope p").text[0...120]
@@ -34,24 +35,14 @@ class Astrological::Scraper
       monthly: monthly_doc.css(".monthlyHoroscope p").text[0...120],
       monthly_url: "http://www.astrology-zodiac-signs.com/horoscope/#{@name.downcase}/monthly/",
       yearly: yearly_doc.css(".astrology .headline-div-block p")[1].text[0...120],
-      yearly_url: "http://www.astrology-zodiac-signs.com/horoscope/#{@name.downcase}/2018/"
+      yearly_url: "http://www.astrology-zodiac-signs.com/horoscope/#{@name.downcase}/#{@current_year}/"
     }
   end
 
   def self.scrape_site_two # cafeastrology.com/
     daily_doc = Nokogiri::HTML(open("https://cafeastrology.com/#{@name.downcase}dailyhoroscope.html", 'User-Agent' => 'chrome'))
     monthly_doc = Nokogiri::HTML(open("https://cafeastrology.com/monthly#{@name.downcase}horoscope.html", 'User-Agent' => 'chrome'))
-    yearly_doc = Nokogiri::HTML(open("https://cafeastrology.com/2018-#{@name.downcase}-horoscope-overview.html", 'User-Agent' => 'chrome')) # Pass in current year?
-
-    # site_two_info = self.new
-    # site_two_info.daily = "It's a mystery!"
-    # site_two_info.daily_url = "https://cafeastrology.com/#{@name.downcase}dailyhoroscope.html"
-    # site_two_info.monthly = monthly_doc.css(".entry-content .content-box-green p").text[0...120]
-    # site_two_info.monthly_url = "https://cafeastrology.com/monthly#{@name.downcase}horoscope.html"
-    # site_two_info.yearly = yearly_doc.css(".entry-content p")[10].text[0...120]
-    # site_two_info.yearly_url = "https://cafeastrology.com/2018-#{@name.downcase}-horoscope-overview.html"
-    #
-    # site_two_info
+    yearly_doc = Nokogiri::HTML(open("https://cafeastrology.com/#{@current_year}-#{@name.downcase}-horoscope-overview.html", 'User-Agent' => 'chrome')) # Pass in current year?
 
     site_two_info = {
       daily: "It's a mystery!",
@@ -59,23 +50,13 @@ class Astrological::Scraper
       monthly: monthly_doc.css(".entry-content .content-box-green p").text[0...120],
       monthly_url: "https://cafeastrology.com/monthly#{@name.downcase}horoscope.html",
       yearly: yearly_doc.css(".entry-content p")[10].text[0...120],
-      yearly_url: "https://cafeastrology.com/2018-#{@name.downcase}-horoscope-overview.html"
+      yearly_url: "https://cafeastrology.com/#{@current_year}-#{@name.downcase}-horoscope-overview.html"
     }
   end
 
   def self.scrape_site_three # astrology.com
     daily_doc = Nokogiri::HTML(open("https://www.astrology.com/horoscope/daily/today/#{@name.downcase}.html", 'User-Agent' => 'chrome'))
-    yearly_doc = Nokogiri::HTML(open("https://www.astrology.com/us/horoscope/yearly-overview-2018.aspx?when=this-year&sign=#{@name.downcase}", 'User-Agent' => 'chrome'))
-
-    # site_three_info = self.new
-    # site_three_info.daily = daily_doc.css(".horoscope .daily-horoscope p").text[0...120]
-    # site_three_info.daily_url = "https://www.astrology.com/horoscope/daily/today/#{@name.downcase}.html"
-    # site_three_info.monthly = nil
-    # site_three_info.monthly_url = nil
-    # site_three_info.yearly = yearly_doc.css(".horoscope .daily-horoscope #overview p").text[0...120]
-    # site_three_info.yearly_url = "https://www.astrology.com/us/horoscope/yearly-overview-2018.aspx?when=this-year&sign=#{@name.downcase}"
-    #
-    # site_three_info
+    yearly_doc = Nokogiri::HTML(open("https://www.astrology.com/us/horoscope/yearly-overview-#{@current_year}.aspx?when=this-year&sign=#{@name.downcase}", 'User-Agent' => 'chrome'))
 
     site_three_info = {
       daily: daily_doc.css(".horoscope .daily-horoscope p").text[0...120],
@@ -83,7 +64,7 @@ class Astrological::Scraper
       monthly: nil,
       monthly_url: nil,
       yearly: yearly_doc.css(".horoscope .daily-horoscope #overview p").text[0...120],
-      yearly_url: "https://www.astrology.com/us/horoscope/yearly-overview-2018.aspx?when=this-year&sign=#{@name.downcase}"
+      yearly_url: "https://www.astrology.com/us/horoscope/yearly-overview-#{@current_year}.aspx?when=this-year&sign=#{@name.downcase}"
     }
   end
 
